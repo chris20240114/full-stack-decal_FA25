9a8d8506da81d6011d04e01448a41838d77f2500
chris <christopher.cs.shen@gmail.com>
2025-10-07 22:45:15 -0700

    vitamin completed

--- vitamin4/script.js
+++ vitamin4/script.js
@@ -2,8 +2,8 @@
  * Question 1
  */
 const question1 = () => {
-  const sidebarButton = __YOUR_CODE_HERE__;
-  const sidebar = __YOUR_CODE_HERE__;
+  const sidebarButton = document.getElementById('sidebar-button');
+  const sidebar = document.getElementById('sidebar');
 
   // Listen for a "click" event on the sidebar's button.
   //
@@ -14,15 +14,16 @@ const question1 = () => {
   // addEventListener will then call the function we provide
   // whenever the button is clicked.
   sidebarButton.addEventListener("click", (event) => {
-    const sidebarIsOpen = __YOUR_CODE_HERE__;
+    const sidebarIsOpen = sidebar.classList.contains("opened");
 
     if (sidebarIsOpen) {
       // Close the sidebar
       /** YOUR CODE HERE */
-      
+      sidebar.classList.remove("opened");
     } else {
       // Open the sidebar
       /** YOUR CODE HERE */
+      sidebar.classList.add("opened");
     }
   });
 };
@@ -31,30 +32,52 @@ const question1 = () => {
  * Question 2
  */
 const question2 = () => {
-  const taskName = __YOUR_CODE_HERE__;
-  const addTodoButton = __YOUR_CODE_HERE__;
-  const todoListUl = __YOUR_CODE_HERE__;
+  const taskName = document.getElementById('task-name');
+  const addTodoButton = document.getElementById('add-todo');
+  const todoListUl = document.getElementById('todo-list');
 
   /** YOUR CODE HERE */
-
+  addTodoButton.addEventListener("click", () => {
+    const task = taskName.value;
+    if(task){
+        const newTodo = document.createElement("li");
+        newTodo.textContent = task;
+        todoListUl.appendChild(newTodo);
+        taskName.value = "";
+    }
+  });
 };
 
 /**
  * Question 3
  */
 const question3 = () => {
-  const firstNameInput = __YOUR_CODE_HERE__;
-  const lastNameInput = __YOUR_CODE_HERE__;
-  const message = __YOUR_CODE_HERE__;
+  const firstNameInput = document.getElementById('first-name');
+  const lastNameInput = document.getElementById('last-name');
+  const message = document.getElementById('message');
 
   // using this function is reccomended but not necessary
   const updateMessage = () => {
     /** YOUR CODE HERE */
-
+    const firstName = firstNameInput.value;
+    const lastName = lastNameInput.value;
+    if(firstName && lastName) {
+        message.textContent = `Hello, ${firstName} ${lastName}!`;
+    } else if (firstName) {
+        message.textContent = `Hello, ${firstName}!`;
+    } else if (lastName) {
+        message.textContent = `Hello, ${lastName}!`;
+    } else {
+        message.textContent = "Hello!";
+    }
   };
 
   /** YOUR CODE HERE */
-
+  firstNameInput.addEventListener("input", updateMessage);
+  lastNameInput.addEventListener("input", updateMessage);
+  
+  // Call it once to initialize the message
+  updateMessage();
 };
 
 /**
